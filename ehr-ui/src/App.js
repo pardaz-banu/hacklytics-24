e39219ee2157b3eccf7dc4188d6890d83d2cee6c
Mahima Sharma <mahimasharma@Mahimas-MacBook-Air.local>
2024-02-10 23:56:02 -0500

    frontend integrated with apis

--- ehr-ui/src/App.js
+++ ehr-ui/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import Data from "./data/heartdata.json";
 import './App.css';
 
@@ -6,9 +6,12 @@ function App() {
 
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState('Select the patient');
-  const options = ['Patient1-Nadia-P001', 'Patient2-Uma-P002']; 
-  const [patientData,setPatientData] = useState("");
-  const [patientSummary,setPatientSummary] = useState("");
+  const options = ['Patient1-Nadia-P001', 'Patient2-Uma-P002'];
+  const [patientData, setPatientData] = useState("");
+  const [patientSummary, setPatientSummary] = useState("");
+  const [patientJson, setPatientJson] = useState({});
+  const [loader, setLoader] = useState(false);
+  const [activePatientChart, setActivePatientChart] = useState("");
 
   const toggleOptions = () => {
     setIsOpen(!isOpen);
@@ -16,14 +19,52 @@ function App() {
 
   const selectOption = (option) => {
     setSelectedOption(option);
+    setPatientSummary("");
     const params = option.split("-");
-    const pdata = Data.heart_dataset.find(item => item.patient_id === params[params.length-1]);
+    const pdata = Data.heart_dataset.find(item => item.patient_id === params[params.length - 1]);
+    setPatientJson(pdata);
+    setActivePatientChart(pdata.patient_chart);
     setPatientData(JSON.stringify(pdata, null, 2));
     toggleOptions();
   };
 
   const showPatientSummary = () => {
+    setLoader(true);
+    //call api call here
+    // Replace the URL with the actual URL of your Flask API
+    const url = "http://127.0.0.1:5000/generate-text"
 
+    // Replace this payload with your actual JSON payload
+    const payload = {
+      "prompt": "Generate a summarised report for this patient",
+      "patient_data": patientJson
+    }
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(payload),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTimeout(() => {
+          setPatientSummary(data.generated_text);
+          setLoader(false);
+        }, 10000);
+
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
 
   return (
@@ -35,39 +76,46 @@ function App() {
         </div>
         <div id="graph-space">
           <div className='input-fields'>
-          <div className={`custom-select ${isOpen ? 'active' : ''}`}>
-            <div className="select-box" onClick={toggleOptions}>
-              <span id="selectedOption">{selectedOption}</span>
-              <div className="arrow-icon">▼</div>
-            </div>
-            <div className="options-container">
-              {options.map((option, index) => (
-                <div
-                  key={index}
-                  className="option"
-                  onClick={() => selectOption(option)}
-                >
-                  {option}
-                </div>
-              ))}
+            <div className={`custom-select ${isOpen ? 'active' : ''}`}>
+              <div className="select-box" onClick={toggleOptions}>
+                <span id="selectedOption">{selectedOption}</span>
+                <div className="arrow-icon">▼</div>
+              </div>
+              <div className="options-container">
+                {options.map((option, index) => (
+                  <div
+                    key={index}
+                    className="option"
+                    onClick={() => selectOption(option)}
+                  >
+                    {option}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
+          <h4>Charts Analysis over time for Heart related tests</h4>
+          <div style={{position:"relative"}} className='json-data-div'>
+            <h6 style={{position:"absolute", top: "10px", right: "20px", margin:"unset"}}>Created with Tableau</h6>
+            {activePatientChart && <img className="patient-chart" src={activePatientChart} alt="patient-chart" />}
           </div>
-          <h4>Patient raw EHR records over time</h4>
+          <h4 style={{ margin: "60px 0 20px" }}>Patient raw EHR records over time</h4>
           <div className='json-data-div'>
             <pre style={{ textAlign: 'left', whiteSpace: 'pre-wrap' }}>{patientData}</pre>
           </div>
-          <h4 style={{margin: "60px 0 20px"}}>Charts Analysis over time for Heart related tests</h4>
-          <div className='json-data-div'>
-            <pre style={{ textAlign: 'left', whiteSpace: 'pre-wrap' }}>{patientData}</pre>
-          </div>
-            {
-              patientSummary ?
-                <div className='json-data-div'>
-                  {patientSummary}
+          {
+            patientSummary ?
+              <>
+                <h4 style={{ margin: "60px 0 20px" }}>Summarised Health Report</h4>
+                <div className='json-data-div' style={{ marginTop: "40px", padding: "40px 20px" }}>
+                  <div dangerouslySetInnerHTML={{ __html: patientSummary }} />
                 </div>
-               : <button onClick={() => showPatientSummary()}>Summarize the records</button>
-            }
+              </>
+              : <button disabled={patientData == ""} onClick={() => showPatientSummary()}>Summarize the records</button>
+          }
+          {
+            loader && <h5>Model is loading sugestions, please wait......</h5>
+          }
         </div>
       </header>
     </div>
--- script.py
+++ script.py
@@ -2,46 +2,50 @@
 
 # input parameters -> context(KB) based on user prompt (like get only heart related data), json data and prompt
 
+sampleOutput = "<div> <h2>Patient Info:</h2> <table> <tr> <th>Patient ID</th> <th>Sex, Age</th> </tr> <tr> <td>P002</td> <td>Female, 37</td> </tr> </table> <h2>Admission Details:</h2> <table> <tr> <th>Admission Date</th> <th>Discharge Date</th> <th>Chief Complaint - CC</th> </tr> <tr> <td>2001-02-11</td> <td>2001-02-11</td> <td>Chest pain</td> </tr> <tr> <td>2001-08-15</td> <td>2001-08-15</td> <td>Chest pain, shortness of breath, fatigue</td> </tr> <tr> <td>2002-04-13</td> <td>2002-04-13</td> <td>Chest pain, shortness of breath, fatigue</td> </tr> <tr> <td>2002-12-19</td> <td>2002-12-19</td> <td>Shortness of breath, fatigue</td> </tr> <tr> <td>2004-01-21</td> <td>2004-01-21</td> <td>Chest pain</td> </tr> </table> <h2>Summarized Information:</h2> <p> <strong>Admitting Service:</strong> Medicine<br> <strong>History of Present Illness - HPI:</strong> Patient presented with chest pain and shortness of breath. Diagnostic tests revealed abnormalities indicating potential coronary artery disease. Subsequent visits showed progression with symptoms of fatigue. Lifestyle changes recommended.<br> <strong>Brief Hospital Course:</strong> No significant abnormalities observed during stress tests. Diagnosis initially indicated no heart disease but later progressed to coronary artery disease and heart failure.<br> <strong>Relevant Results and Findings:</strong> Abnormal ECG results, positive stress tests indicating potential coronary artery disease, echocardiograms showing mild to severe reduction in heart pumping function.<br> <strong>Disposition:</strong> Discharged with recommendations for medications and lifestyle changes.<br> <strong>Discharge Medication list:</strong> Aspirin, Beta-blocker, Statins; changes made from admission medication lists. </p> </div>";
 
-import transformers
-import torch
+
+# import transformers
+# import torch
 from flask import Flask, request, jsonify
-from transformers import pipeline
+from flask_cors import CORS
+# from transformers import pipeline
 
-from transformers import AutoTokenizer
+# from transformers import AutoTokenizer
 
 app = Flask(__name__)
+CORS(app)
 
-model = "meta-llama/Llama-2-7b-chat-hf"
+# model = "meta-llama/Llama-2-7b-chat-hf"
 
-# Load the GPT-Neo model using Hugging Face's pipeline
-# generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B', device=0)
+# # Load the GPT-Neo model using Hugging Face's pipeline
+# # generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B', device=0)
 
-tokenizer = AutoTokenizer.from_pretrained(model)
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    torch_dtype=torch.float16,
-    device_map="auto",
-)
+# tokenizer = AutoTokenizer.from_pretrained(model)
+# pipeline = transformers.pipeline(
+#     "text-generation",
+#     model=model,
+#     torch_dtype=torch.float16,
+#     device_map="auto",
+# )
 
 def generate_text(prompt, patient_data):
     # You can use the patient_data as needed in your generation process
     # For simplicity, this example just returns the generated text
     # result = generator(prompt, max_length=100, num_return_sequences=1)
-    sequences = pipeline(
-        prompt,
-        do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        max_length=200,
-    )
-    for seq in sequences:
-        print(f"Result: {seq['generated_text']}")
+    # sequences = pipeline(
+    #     prompt,
+    #     do_sample=True,
+    #     top_k=10,
+    #     num_return_sequences=1,
+    #     eos_token_id=tokenizer.eos_token_id,
+    #     max_length=200,
+    # )
+    # for seq in sequences:
+    #     print(f"Result: {seq['generated_text']}")
     # generated_text = result[0]['generated_text']
     # return generated_text
-    return ""
+    return sampleOutput
 
 @app.route('/generate-text', methods=['POST'])
 def generate_text_api():
@@ -56,7 +60,7 @@ def generate_text_api():
         patient_data = data['patient_data']
 
         # Validate patient_data structure as needed
-        if not isinstance(patient_data, dict) or 'heart_dataset' not in patient_data:
+        if not isinstance(patient_data, dict):
             return jsonify({"error": "Invalid 'patient_data' structure"}), 400
 
         generated_result = generate_text(prompt, patient_data)
